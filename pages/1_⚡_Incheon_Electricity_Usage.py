39c10bb1d74de4e858ce58c3f3df13980c356aeb
dakoyaki <58243830+dakoyaki@users.noreply.github.com>
2023-01-25 18:58:15 +0900

    Update 1_⚡_Incheon_Electricity_Usage.py

--- pages/1_⚡_Incheon_Electricity_Usage.py
+++ pages/1_⚡_Incheon_Electricity_Usage.py
@@ -34,7 +34,7 @@ def get_data_columns(df):
             cols.remove(col)
     return cols
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def get_geom_data(path):
     gdf = gpd.read_file(path)
     return gdf
